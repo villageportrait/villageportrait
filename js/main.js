2b0e3b43c9d1d00ec4e7c7af86dff4d0937af5f2
villageportrait <jesse@villageportrait.com>
2025-03-27 23:23:24 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// Main JavaScript for Village Portrait Website
-
-document.addEventListener('DOMContentLoaded', () => {
-    // No need to initialize slideshow here as it's handled in slideshow.js
-});
